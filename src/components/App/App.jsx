f84548710692abe366dcbe4fb573002aa0478a21
MariOkner <MariOkner@gmail.com>
2022-10-25 14:06:34 +0300

    додала localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -23,12 +23,7 @@ export class App extends Component {
   };
 
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -62,6 +57,23 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    };
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+
+    if (this.state.contacts !== prevState.contacts) {
+      console.log("Обновилось поле контактов");
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  };
+
   render() {
     return (
       <Container>
@@ -80,3 +92,8 @@ export class App extends Component {
     );
   }
 }
+
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
